7d98d9452368ef55e08bf7a83c4985b05a67c6f1
i.a.grekhov <iagrekhov.fronted@gmail.com>
2025-07-06 01:23:56 +0500

    Add descriptor info

---
+++ javascript-common/descriptors.js
@@ -0,0 +1,45 @@
+console.log("============================================");
+console.log("objectWithGetSetDescriptor");
+
+const objectWithGetSetDescriptor = {};
+
+Object.defineProperty(
+  objectWithGetSetDescriptor,
+  "viewFieldWithGetter",
+  (() => {
+    let value = "";
+
+    return {
+      // Геттер позволяет "на лету" преобразовывать значение свойства
+      get: () => `Value: ${value}`,
+      // Сеттер позволяет устанавливать значение. Необходим для работы геттера
+      set: (val) => (value = val),
+      // По умолчанию всегда true - определяет видимость ключа
+      enumerable: true,
+      // Позволяет управлять свойством - удалять и менять его конфигурацию
+      configurable: true,
+    };
+  })()
+);
+
+objectWithGetSetDescriptor.viewFieldWithGetter = "test";
+
+console.log(JSON.stringify(objectWithGetSetDescriptor, null, 2));
+console.log(objectWithGetSetDescriptor.viewFieldWithGetter);
+
+console.log("============================================");
+console.log("objectWithHiddenProperties");
+
+const objectWithHiddenProperties = {};
+
+Object.defineProperty(objectWithHiddenProperties, "hiddenField", {
+  enumerable: false,
+  configurable: false,
+  // Позволяет изменять значение свойства
+  writable: true,
+  value: "hidden",
+});
+
+console.log(JSON.stringify(objectWithHiddenProperties, null, 2));
+console.log(objectWithHiddenProperties.hiddenField);
+console.log(Reflect.ownKeys(objectWithHiddenProperties));
